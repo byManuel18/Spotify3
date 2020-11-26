be010dd7f4141810fdcdb6dff4b1909e122e4567
Manuel <antoniomanu533@gmail.com>
2020-11-26 12:40:19 +0100

    Inicio

---
+++ Spotify3/src/main/java/discography/Spotify3/App.java
@@ -0,0 +1,13 @@
+package discography.Spotify3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
