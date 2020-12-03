4c66c206085adaaad8c743660fd701899bd84a58
Luis Daniel Ortega Pedrajas <alortega@iesfranciscodelosrios.es>
2020-12-03 11:06:15 +0100

    Genre

--- Spotify3/src/main/java/model/Genre.java
+++ Spotify3/src/main/java/model/Genre.java
@@ -1,8 +1,27 @@
 package model;
 
-public class Genre{
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="GENRE")
+public class Genre implements Serializable{
+	
+	
+	private static final long serialVersionUID=1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="ID")
 	private int id;
+	@Column(name="NAME")
 	private String name;
 
 	public Genre(){
